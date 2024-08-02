fixed two step lookahead minimization

runSeqRollout.py
@@ -60,7 +60,7 @@ def create_agents(
 
 if __name__ == '__main__':
     env = gym.make(SpiderAndFlyEnv)
-    wandb.init(project="SecurityAndSurveillance",name="Sequential Rollout")
+    wandb.init(project="SecurityAndSurveillance",name="Seq_Roll_2StepLookahead")
     
     for epi in range (N_EPISODES):
         frames = []
@@ -72,6 +72,7 @@ if __name__ == '__main__':
         total_reward = 0.
         epi_steps = 0
         while not all(done_n):
+            
             prev_actions = {}
             act_n = []
             for i, (agent, obs) in enumerate(zip(agents, obs_n)):
@@ -83,15 +84,15 @@ if __name__ == '__main__':
 
 
             obs_n, reward_n, done_n, info = env.step(act_n)
+            print(f"taking Step {act_n}")
             epi_steps += 1
-            total_reward += np.sum(reward_n)
+            total_reward += np.mean(reward_n)
             frames.append(env.render())
 
         endTime = time.time()
 
         print(f'Episode {epi}: Reward is {total_reward}, with steps {epi_steps} exeTime{endTime-startTime}')
-        time.sleep(6)
-        wandb.log({'Reward':total_reward, 'episode_steps' : epi_steps,'exeTime':endTime-startTime-6},step=epi) 
+        wandb.log({'Reward':total_reward, 'episode_steps' : epi_steps,'exeTime':endTime-startTime},step=epi) 
         
 
 
@@ -101,5 +102,5 @@ if __name__ == '__main__':
             # create_movie_clip(frames, f"ManhattanRuleBased_2_agents_{epi+1}.mp4", fps=10)
             wandb.log({"video": wandb.Video(np.stack(frames,0).transpose(0,3,1,2), fps=20,format="mp4")})
 
-    wandb.finish()
+    # wandb.finish()
     env.close()

src/agent_seq_rollout.py
@@ -1,5 +1,5 @@
 from typing import List, Dict, Tuple
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed, ThreadPoolExecutor
 
 import numpy as np
 import gym
@@ -9,6 +9,8 @@ from src.constants import SpiderAndFlyEnv, AgentType
 from src.agent_rule_based import RuleBasedAgent
 from src.agent_qnet_based import QnetBasedAgent
 from src.agent import Agent
+import itertools
+import pandas as pd
 
 
 class SeqRolloutAgent(Agent):
@@ -39,7 +41,7 @@ class SeqRolloutAgent(Agent):
             prev_actions: Dict[int, int] = None,
             **kwargs,
     ) -> int:
-        best_action, action_q_values = self.act_with_info(obs, prev_actions)
+        best_action = self.act_with_info_2Step(obs, prev_actions)
         return best_action
 
     def act_with_info1(
@@ -84,6 +86,51 @@ class SeqRolloutAgent(Agent):
         best_action = np.argmax(action_q_values)
 
         return best_action, action_q_values
+    
+    def act_with_info_2Step(
+            self,
+            obs: List[float],
+            prev_actions: Dict[int, int] = None,
+        ) -> Tuple[int, np.ndarray]:
+        assert prev_actions is not None
+
+        n_actions = self._action_space.n
+
+        # Function to simulate action and return results
+        def simulate_action(action_id):
+            print(f"finding rewards for action {action_id}")
+            # perform simulation
+            action_id, lsitof2ndStepRewards = self._simulate_action_par_2Step(
+                self.id,
+                action_id,
+                self._n_sim_per_step,
+                obs,
+                prev_actions,
+                self._m_agents,
+                self._agents,
+            )
+            return lsitof2ndStepRewards
+
+        # Parallel calculations
+        sim_results = []
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            # Submit tasks
+            future_to_action = {executor.submit(simulate_action, action_id): action_id for action_id in range(n_actions)}
+
+            # Collect results as they complete
+            for future in as_completed(future_to_action):
+                action_id = future_to_action[future]
+                try:
+                    lsitof2ndStepRewards = future.result()
+                    sim_results += lsitof2ndStepRewards
+                except Exception as exc:
+                    print(f"Action {action_id} generated an exception: {exc}")
+
+        # Convert results to DataFrame and find the best action
+        dfSimResults = pd.DataFrame(sim_results)
+        dfSimResultsSorted = dfSimResults.sort_values(by="reward", ascending=False)
+        bestAction = dfSimResultsSorted.iloc[0]["action_id"]
+        return bestAction
     
     def act_with_info(
             ## without pralallel processing
@@ -205,3 +252,95 @@ class SeqRolloutAgent(Agent):
         avg_total_reward /= n_sims
 
         return action_id, avg_total_reward
+    
+
+
+
+    @staticmethod
+    def _simulate_action_par_2Step(
+            agent_id: int,
+            action_id: int,
+            n_sims: int,
+            obs: List[float],
+            prev_actions: Dict[int, int],
+            m_agents: int,
+            agents: List[Agent],
+    ) -> Tuple[int, float]:
+        # Memory and CPU load
+        # create env
+        # run N simulations
+
+        # create env
+        env = gym.make(SpiderAndFlyEnv)
+
+        # roll first step
+        first_step_prev_actions = dict(prev_actions)
+        first_act_n = np.empty((m_agents,), dtype=np.int8)
+        for i in range(m_agents):
+            if i in prev_actions:
+                # if ith agent's (other agents) previous action is available
+                # use it for env.step
+                first_act_n[i] = prev_actions[i]
+            elif agent_id == i:
+                # if optimization is done for me (not other agents)
+                # use my action for env.step
+                first_act_n[i] = action_id
+                first_step_prev_actions[i] = action_id
+            else:
+                # if other agents previous actions are not available
+                # assume action based on base policy
+                underlying_agent = agents[i]
+                assumed_action = underlying_agent.act(obs, prev_actions=first_step_prev_actions)
+                first_act_n[i] = assumed_action
+                first_step_prev_actions[i] = assumed_action
+
+
+
+        # first prescribed step is  first_act_n
+        # take the prescribed step to create a new observation
+        env.reset()
+        sim_obs_n = env.reset_from(obs)
+        step1_obs_n, step1_reward_n, step1_done_n, step1_info = env.step(first_act_n)
+        
+        # generate the 5**4 next action options
+        combinationsForSecondStep = list(itertools.product(range(5), repeat=4))
+        lsitof2ndStepRewards = []
+
+        for first_act_nTUple in combinationsForSecondStep:
+            second_act_n = list(first_act_nTUple)
+            # run N simulations
+            avg_total_reward = 0.
+
+            for j in range(n_sims):
+                # init env from observation
+                env.reset()
+                sim_obs_n = env.reset_from(step1_obs_n[0])
+
+                # make prescribed first step
+                sim_obs_n, sim_reward_n, sim_done_n, sim_info = env.step(second_act_n)
+                avg_total_reward += np.sum(sim_reward_n)
+
+                # run simulation
+                while not all(sim_done_n):
+                    sim_act_n = []
+                    sim_prev_actions = {}
+                    for agent, sim_obs in zip(agents, sim_obs_n):
+                        sim_best_action = agent.act(sim_obs, prev_actions=sim_prev_actions)
+                        sim_act_n.append(sim_best_action)
+                        sim_prev_actions[agent.id] = sim_best_action
+
+                    sim_obs_n, sim_reward_n, sim_done_n, sim_info = env.step(sim_act_n)
+                    avg_total_reward += np.sum(sim_reward_n)
+
+            
+
+        
+
+            avg_total_reward /= len(agents)
+            avg_total_reward /= n_sims
+
+
+            lsitof2ndStepRewards.append({"reward":avg_total_reward, "step2" : second_act_n,"step1":first_act_n, "action_id":action_id})
+
+
+        return action_id, lsitof2ndStepRewards